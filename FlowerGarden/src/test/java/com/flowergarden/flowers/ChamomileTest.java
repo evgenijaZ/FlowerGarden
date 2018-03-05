7e58937dd06ac93511411f495d248d511c96d601
evgenijaZ <evg.zubrich@gmail.com>
2018-03-06 00:39:52 +0200

    added mocked fressness

--- FlowerGarden/src/test/java/com/flowergarden/flowers/ChamomileTest.java
+++ FlowerGarden/src/test/java/com/flowergarden/flowers/ChamomileTest.java
@@ -2,26 +2,36 @@ package com.flowergarden.flowers;
 
 import com.flowergarden.properties.FreshnessInteger;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 /**
  * @author Yevheniia Zubrych on 04.03.2018.
  */
+
 public class ChamomileTest {
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private FreshnessInteger freshness;
 
     private Chamomile chamomile;
 
     @Test
     public void testGettingPetal() throws Exception {
         //Given
-        chamomile = new Chamomile(2, 20, 5, new FreshnessInteger(5));
+        chamomile = new Chamomile(2, 20, 5, freshness);
         Assert.assertEquals(true, chamomile.getPetal());
     }
 
     @Test
     public void testGettingTheLastPetal() throws Exception {
         //Given
-        chamomile = new Chamomile(0, 20, 5, new FreshnessInteger(5));
+        chamomile = new Chamomile(0, 20, 5, freshness);
         //When
         boolean petalIsObtained = chamomile.getPetal();
         //Then
@@ -31,18 +41,18 @@ public class ChamomileTest {
     @Test(expected = IllegalArgumentException.class)
     public void testNegativePetals() throws Exception {
         //Given
-        chamomile = new Chamomile(-1, 20, 5, new FreshnessInteger(5));
+        chamomile = new Chamomile(-1, 20, 5, freshness);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testNegativeLength() throws Exception {
         //Given
-        chamomile = new Chamomile(10, -20, 5, new FreshnessInteger(5));
+        chamomile = new Chamomile(10, -20, 5, freshness);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testNegativePrice() throws Exception {
         //Given
-        chamomile = new Chamomile(10, 20, -5, new FreshnessInteger(5));
+        chamomile = new Chamomile(10, 20, -5, freshness);
     }
 }
