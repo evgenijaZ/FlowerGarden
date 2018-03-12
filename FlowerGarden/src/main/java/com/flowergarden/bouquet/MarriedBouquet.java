2d0555a818cbc8b849f86e881aeb07b9d15c39a4
evgenijaZ <evg.zubrich@gmail.com>
2018-03-12 19:14:09 +0200

    the identifier was added and the main properties were placed in a separate class

--- FlowerGarden/src/main/java/com/flowergarden/bouquet/MarriedBouquet.java
+++ FlowerGarden/src/main/java/com/flowergarden/bouquet/MarriedBouquet.java
@@ -1,50 +1,12 @@
 package com.flowergarden.bouquet;
 
-import com.flowergarden.flowers.GeneralFlower;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
-public class MarriedBouquet implements Bouquet <GeneralFlower> {
+public class MarriedBouquet extends GeneralBouquet {
 
     private float assemblePrice = 120;
-    private List <GeneralFlower> flowerList = new ArrayList <>();
 
     @Override
     public float getPrice() {
-        float price = assemblePrice;
-        for (GeneralFlower flower : flowerList) {
-            price += flower.getPrice();
-        }
-        return price;
-    }
-
-    @Override
-    public void addFlower(GeneralFlower flower) {
-        flowerList.add(flower);
-    }
-
-    @Override
-    public Collection <GeneralFlower> searchFlowersByLength(int start, int end) {
-        List <GeneralFlower> searchResult = new ArrayList <>();
-        for (GeneralFlower flower : flowerList) {
-            if (flower.getLength() >= start && flower.getLength() <= end) {
-                searchResult.add(flower);
-            }
-        }
-        return searchResult;
-    }
-
-    @Override
-    public void sortByFreshness() {
-        Collections.sort(flowerList);
-    }
-
-    @Override
-    public Collection <GeneralFlower> getFlowers() {
-        return flowerList;
+        return super.getPrice() + assemblePrice;
     }
 
     public void setAssembledPrice(float price) {
