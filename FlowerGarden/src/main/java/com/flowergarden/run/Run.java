d088092fc6652f12826c2528d759c8435cede55a
evgenijaZ <evg.zubrich@gmail.com>
2018-03-14 10:20:13 +0200

    added business logic

--- FlowerGarden/src/main/java/com/flowergarden/run/Run.java
+++ FlowerGarden/src/main/java/com/flowergarden/run/Run.java
@@ -1,9 +1,23 @@
 package com.flowergarden.run;
 
+import com.flowergarden.bouquet.GeneralBouquet;
+import com.flowergarden.dao.BouquetDAO;
+import com.flowergarden.dao.FlowerDAO;
+import com.flowergarden.flowers.GeneralFlower;
+
+import java.util.List;
+
 public class Run {
 
     public static void main(String[] args) {
 
-
+        BouquetDAO bouquetDAO = new BouquetDAO();
+        FlowerDAO flowerDAO = new FlowerDAO();
+        GeneralBouquet bouquet = bouquetDAO.getByKey(1);
+        List <GeneralFlower> flowers = flowerDAO.getByBouquetId(1);
+        for (GeneralFlower flower : flowers) {
+            bouquet.addFlower(flower);
+        }
+        System.out.println(bouquet.getPrice());
     }
 }
