636b0ccb5f0b38d143727b3c524fdb225e8bce6a
evgenijaZ <evg.zubrich@gmail.com>
2018-03-19 21:55:36 +0200

    added BouquetDAO

--- FlowerGarden/src/main/java/com/flowergarden/dao/BouquetDAO.java
+++ FlowerGarden/src/main/java/com/flowergarden/dao/BouquetDAO.java
@@ -1,8 +1,51 @@
 package com.flowergarden.dao;
 
+import com.flowergarden.bouquet.GeneralBouquet;
+import com.flowergarden.bouquet.MarriedBouquet;
+
 /**
  * @author Yevheniia Zubrych on 12.03.2018.
  */
-public class BouquetDAO {
+public class BouquetDAO extends DAO<MarriedBouquet,Integer> {
 
+    String UPDATE = "UPDATE %s.%s SET %s=? WHERE %s=?;";
+    private String INSERT = "INSERT INTO %s.%s (%s) VALUES (?);";
+    private String INSERT_BY_ID = "INSERT INTO %s.%s (%s, %s) VALUES (?, ?);";
+
+    private String[][] nameMapping = {{"assemblePrice","assemble_price"},{"id","id"}};
+
+    public BouquetDAO(String dbName, String schemaName, String tableName) {
+        super(dbName, schemaName, tableName);
+    }
+
+    @Override
+    protected Class <MarriedBouquet> getEntityClass() {
+        return MarriedBouquet.class;
+    }
+
+    @Override
+    protected Class <Integer> getKeyClass() {
+        return Integer.TYPE;
+    }
+
+    @Override
+    public String[][] getNameMapping() {
+        return nameMapping;
+    }
+
+    @Override
+    String getUpdateQuery() {
+        return String.format(this.UPDATE, makeFormatArgs(getFieldCount(), getNameMapping()));
+    }
+
+    @Override
+    public String getInsertQuery() {
+        return String.format(INSERT, makeFormatArgs(getFieldCount() - 1, getNameMapping()));
+
+    }
+
+    @Override
+    public String getInsertByIdQuery() {
+        return String.format(INSERT_BY_ID, makeFormatArgs(getFieldCount(), getNameMapping()));
+    }
 }
