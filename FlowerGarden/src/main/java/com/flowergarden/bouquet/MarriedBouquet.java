77ead799f59ade997d46364d6c9dd1035f517c3d
evgenijaZ <evg.zubrich@gmail.com>
2018-03-21 23:19:11 +0200

    added id, overridden equals()

--- FlowerGarden/src/main/java/com/flowergarden/bouquet/MarriedBouquet.java
+++ FlowerGarden/src/main/java/com/flowergarden/bouquet/MarriedBouquet.java
@@ -1,15 +1,22 @@
 package com.flowergarden.bouquet;
 
+import com.flowergarden.flowers.GeneralFlower;
+
+import java.util.Objects;
+
 public class MarriedBouquet extends GeneralBouquet {
 
+    private int id;
     private float assemblePrice;
 
     public MarriedBouquet() {
         this.assemblePrice = 120;
+        this.id = -1;
     }
 
     public MarriedBouquet(float assemblePrice) {
         this.assemblePrice = assemblePrice;
+        this.id = -1;
     }
 
     public MarriedBouquet(int id, float assemblePrice){
@@ -28,5 +35,25 @@ public class MarriedBouquet extends GeneralBouquet {
 
     public float getAssemblePrice() {
         return assemblePrice;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof MarriedBouquet))
+            return false;
+        MarriedBouquet bouquet = (MarriedBouquet) obj;
+        return (this.assemblePrice == bouquet.assemblePrice)
+                && Objects.equals(this.getFlowers(), bouquet.getFlowers());
     }
 }
